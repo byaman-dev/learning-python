ec08971ca06377e3a19722547f4f2e05a45151a6
Aman Deshlahre <byaman.dev@gmail.com>
2026-04-06 12:00:02 +0530

    Practical 3 - set

---
+++ Chapter 4: Lists & Tuples/03_practice_set_solutions.py
@@ -0,0 +1,24 @@
+# --- Practice Set Solutions ---
+
+# Task 1: Store 7 fruits entered by the user
+fruits = []
+f1 = input("Enter Fruit 1: ")
+fruits.append(f1)
+f2 = input("Enter Fruit 2: ")
+fruits.append(f2)
+f3 = input("Enter Fruit 3: ")
+fruits.append(f3)
+f4 = input("Enter Fruit 4: ")
+fruits.append(f4)
+f5 = input("Enter Fruit 5: ")
+fruits.append(f5)
+f6 = input("Enter Fruit 6: ")
+fruits.append(f6)
+f7 = input("Enter Fruit 7: ")
+fruits.append(f7)
+print(f"Your Fruit List: {fruits}")
+
+# Task 2: Sum a list with 4 numbers
+numbers = [12, 45, 67, 23]
+total = sum(numbers)
+print(f"The total sum of the list is: {total}")

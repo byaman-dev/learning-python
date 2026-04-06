dd6d9da5b8c949a500bc5dff73a1256e44772d8c
Aman Deshlahre <byaman.dev@gmail.com>
2026-04-06 21:32:28 +0530

    Python Variable Naming Convention Lab

---
+++ Chapter-04-Data-Collections/04_02_variable_naming_rules.py
@@ -0,0 +1,15 @@
+# --- Python Variable Naming Convention Lab ---
+
+# ✅ LEGAL NAMES
+user_name = "Aman"     # Snake case (Professional)
+_secret_key = "Ax89j"  # Starts with underscore (Internal use)
+port22 = "SSH"         # Numbers at the end are fine
+AI_SECURITY = "Active" # Uppercase for constants
+
+# ❌ ILLEGAL NAMES (Uncomment to see the error)
+# 22port = "Error"     # Cannot start with a number
+# user-name = "Error"   # Cannot use hyphens in names
+# @status = "Error"    # No special symbols except _
+# class = "Error"       # Cannot use Reserved Keywords
+
+print("Successfully validated all legal variable names.")
